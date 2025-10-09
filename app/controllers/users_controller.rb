9953296d0ebfd7769524ad4b08efbbeb846be9b0
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-09 16:28:25 -0600

    fix: syntax error in form_with & remove params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-    # @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to new_user_path, notice: "User created successfully!"
     else
       render :new, status: :unprocessable_entity
     end
