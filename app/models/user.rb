26c24766cc404ca6c94a594fa449ab33363e90b0
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-10-29 10:46:36 -0600

    style: use rubocop to fix style problems

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 4, maximum: 20 },
-                       format: { with: /\A[A-Za-z0-9_]+\z/, 
+                       format: { with: /\A[A-Za-z0-9_]+\z/,
                                 message: "only allows letters, numbers, and underscores" }
-  
+
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
-  
+
   validates :password, length: { minimum: 6, maximum: 30 }, if: :password_digest_changed?
 end
